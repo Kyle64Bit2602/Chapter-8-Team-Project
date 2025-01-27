877fe5a85c5b378ff20bb45d1b650a56f79e4d43
Kyle64Bit2602 <kylebackup64@gmail.com>
2025-01-27 14:46:06 -0600

    Update Chapter8TeamProject.cpp

--- Chapter8TeamProject.cpp
+++ Chapter8TeamProject.cpp
@@ -1,2 +1,27 @@
 //Chapter 8 Team Project
 //Kyle, Kade, Cole, Jullian
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+//Function Prototypes (arguments, type of functions to be updated)
+void input_grades();
+void view_grades();
+void student_data();
+void data_analysis();
+void quick_sort();
+void merge_sort();
+void binary_search();
+void menu();
+
+/*
+################
+######Main######
+################
+*/
+int main()
+{
+	
+}
