05f7cf46617e1f1afe30c90b0d38f84284507cf6
Kyle64Bit2602 <kylebackup64@gmail.com>
2025-01-28 14:35:18 -0600

    Update Chapter8TeamProject.cpp

--- Chapter8TeamProject.cpp
+++ Chapter8TeamProject.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <string>
 #include <iomanip>
+#include <fstream>
 using namespace std;
 
 //Function Prototypes (arguments, type of functions to be updated)
@@ -14,6 +15,8 @@ void data_analysis();
 void quick_sort();
 void merge_sort();
 void binary_search();
+void load_file();
+void save_to_file();
 void menu();
 
 /*
@@ -24,4 +27,49 @@ void menu();
 int main()
 {
 	
+}
+
+void load_file()
+{
+	string filename;
+	cout << "Please enter the file location you wish to load into the program.\n";
+	cout << "(Add another \ to each location.\n)";
+	cout << "(Ex: C:\\Users\\BillyBob\\Desktop\\Chapter-8-Team-Project)\n";
+	cout << ">>: ";
+	cin >> filename;
+
+	ifstream file(filename);
+	
+	if (!file)
+	{
+		cout << "Error: File was not found.\n"
+	}
+	else
+	{
+		//Placeholder
+		//Loads file into data in array format
+	}
+}
+
+void save_to_file(array[])
+{
+	string filename;
+	cout << "Please enter the file location you wish to save to.\n";
+	cout << "(Add another \ to each location.\n)";
+	cout << "(Ex: C:\\Users\\BillyBob\\Desktop\\Chapter-8-Team-Project)\n";
+	cout << ">>: ";
+	cin >> filename;
+
+	ofstream file(filename);
+
+	if (!file)
+	{
+		cout << "Error: File was not found.\n"
+	}
+	else
+	{
+		//Placeholder
+		//Will take the array of student grades to save
+	}
+
 }
