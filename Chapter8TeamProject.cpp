3c39bc17621b0918e2c6369f2c4d8fdf5cf02228
Kyle64Bit2602 <kylebackup64@gmail.com>
2025-02-03 14:49:47 -0600

    vf

--- Chapter8TeamProject.cpp
+++ Chapter8TeamProject.cpp
@@ -9,9 +9,9 @@ using namespace std;
 
 //Function Prototypes (arguments, type of functions to be updated)
 void input_grades();
-void view_grades();
 void student_data();
 void quickSort(int arr[], int lowIndex, int highIndex)
+void view_grades(int grades[], string students[])
 void merge_sort();
 void load_file();
 void save_to_file(array[]);
@@ -28,9 +28,53 @@ double calculate_average(int grades[], int grade_count);
 */
 int main()
 {
+	char cont;
+	bool check = True;
+	while (check == True)
+	{
+		choice = menu();
+		if (choice == 1)
+		{
+			pair<int[], string[]> gradesStudents = input_grades();
+			int grades[] = gradesStudents.first;
+			string students[] = gradesStudents.second;
+		}
+		else if (choice == 2)
+		{
+			view_grades(int grades[], string students[])
+		}
 	
+	}
 }
+int menu()
+{
+	int choice = 0;
+	bool check = false;
 
+	cout << "Student Grades Management System\n";
+	cout << "1. Input Grades\n";
+	cout << "2. View Grades\n";
+	cout << "3. Save Grades to File\n";
+	cout << "4. Load Grades from File\n";
+	cout << "5. Analyze Grades\n";
+	cout << "6. Search for a Student by Name\n";
+	cout << "7. Sort Data\n";
+	cout << "8. Exit\n";
+	cout << "\n\nEnter your choice:";
+	cin >> choice;
+	while (check == false)
+	{
+		if (choice < 1 || choice > 8)
+		{
+			cout << "Error: Input does not fall between 1-8.\n";
+			cout << "Enter your choice: ";
+			cin >> choice;
+		}
+		else
+			check == true
+	}
+	return choice
+}
 void load_file()
 {
 	string filename;
@@ -53,7 +97,7 @@ void load_file()
 	}
 }
 
-void save_to_file(array[])
+void save_to_file(int grades[], string students[])
 {
 	string filename;
 	cout << "Please enter the file location you wish to save to.\n";
@@ -70,34 +114,50 @@ void save_to_file(array[])
 	}
 	else
 	{
-		//Placeholder
-		//Will take the array of student grades to save
+		for (int index = 0, index < size; index++)
 	}
 
 }
 
-void input_grades()
+pair <int[], string[]> input_grades()
 {
 	//initiates variables
+	bool check = True;
+	char cont;
 	string student;
+	string students[];
 	int grade;
-	int grades[4];
-	cout << "Enter a student's name: "; //gets a 
-	getline(cin, student);
-	for (int n = 0; n > 5, n++)
+	int grades[];
+	while (check == True)
 	{
-		cout << "Grade " << n << ":";
-		cout << grades[n];
+		cout << "Enter a student's name: ";
+		getline(cin, student);
+		int num;
+		cout << "How many grades do you want to input?\n";
+		cin >> num;
+		for (int n = 0; n = num; n++)
+		{
+			cout << "Grade " << n << ":";
+			cin >> n;
+			cout << grades[n];
+		}
+		cout << "Grades successfully added.\n";
+		cout << "Would you like to add another student to the registry? (y/n)";
+		cin >> cont;
+		if (cont != 'y' || cont != 'Y')
+		{
+			check = False;
+		}
 	}
-	
+	return {grades[], students[])
 }
 
-void view_grades(arr[])
+void view_grades(int grades[], string students[])
 {
 	int size = sizeof(arr) / sizeof(arr[0]); // Calculate array size
 	for (int index = 0; index < size; index++)
 	{
-		cout << arr[index] << endl;
+		cout << students[index] << \t\t\t << arr[index] << endl;
 	}
 }
 
@@ -137,35 +197,6 @@ int partition(int arr[], int lowIndex, int highIndex) //Splits data into two for
 		}
 	}
 }
-
-int menu()
-{
-	int choice = 0;
-	bool check = false;
-
-	cout << "Student Grades Management System\n";
-	cout << "1. Input Grades\n";
-	cout << "2. View Grades\n";
-	cout << "3. Save Grades to File\n";
-	cout << "4. Load Grades from File\n";
-	cout << "5. Analyze Grades\n";
-	cout << "6. Search for a Student by Name\n";
-	cout << "7. Sort Data\n";
-	cout << "8. Exit\n";
-
-	cout << "\n\nEnter your choice:";
-	cin >> choice;
-	while (check == false)
-	{
-		if (choice < 1 || choice > 8)
-		{
-			cout << "Error: Input does not fall between 1-8.\n";
-			cout << "Enter your choice: ";
-			cin >> choice;
-		}
-		else
-			check == true
-	}
 
 // Function to analyze data: calculate averages, find min and max
 void data_analysis(string students[], int* grades[], int student_count, int grade_counts[]) {
